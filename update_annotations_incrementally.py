62612d7c09e00ea0556e8389b5fb63723a1f7ade
Francis Chan <franciszero@gmail.com>
2023-03-18 16:55:02 -0400

    .

--- update_annotations_incrementally.py
+++ update_annotations_incrementally.py
@@ -1,8 +1,14 @@
 from models.YOLOv8 import AnnotationYOLOv8
 from models.ResNet import AnnotationResNet
 from models.InsightFace import AnnotationInsightFace
+import sys
 
-batch_path = "./data/batch_20230318_2/"
+batch_path = None
+if len(sys.argv) == 2:
+    batch_path = str(sys.argv[1])  # e.g. "./data/batch_20230318_0/"
+else:
+    exit(250)
+
 AnnotationResNet(batch_path, model_id=0).foo()
 AnnotationYOLOv8(batch_path, model_id=1).foo()
 AnnotationInsightFace(batch_path, model_id=2).foo()
