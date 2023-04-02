d3785bb88e28c707fe4a268ff382949c1b1732b6
Francis Chan <franciszero@gmail.com>
2023-04-02 11:15:53 -0400

    .

--- data/img_class_ground_truth/Model predictions comparison.py
+++ data/img_class_ground_truth/Model predictions comparison.py
@@ -20,20 +20,20 @@ pd.set_option('display.max_columns', 30)
 pd.set_option('display.width', 2000)
 pd.set_option('display.precision', 3)
 
-test = pd.DataFrame(columns=["filename", "ground_truth", "train18", "train31"], dtype=int, )
+test = pd.DataFrame(columns=["Image", "ground truth", "train18", "train31"], dtype=int, )
 ds16 = fo.load_dataset('YOLOv8x_BB158_500_t16_b')
 ds31 = fo.load_dataset('YOLOv8x_BB426_760_t31_b')
 for i, (s16, s31) in enumerate(zip(ds16, ds31)):
     if s16.filename == s31.filename:
         idx = test.index.values.size
-        test.loc[idx, "filename"] = s31.filename
+        test.loc[idx, "Image"] = s31.filename
         if s16.detections is None and s31.detections is None:
-            test.loc[idx, "ground_truth"] = 0
+            test.loc[idx, "ground truth"] = 0
         else:
             gt16 = s16.detections.detections
             gt31 = s31.detections.detections
             if len(gt16) == len(gt31):
-                test.loc[idx, "ground_truth"] = len(gt16)
+                test.loc[idx, "ground truth"] = len(gt16)
             else:
                 print(s16.filename, len(gt16), " != ", len(gt31))
 
@@ -59,20 +59,20 @@ for i, (s16, s31) in enumerate(zip(ds16, ds31)):
               len(s31.predictions.detections), )
         break
 
-test_sort = test.sort_values(by=['ground_truth', 'train31', 'train18'], ascending=False)
-tmp = test_sort.set_index(['filename']).stack()
-tmp = tmp.rename_axis(index=['filename', 'class'])
-tmp.name = 'value'
+test_sort = test.sort_values(by=['ground truth', 'train31', 'train18'], ascending=False)
+tmp = test_sort.set_index(['Image']).stack()
+tmp = tmp.rename_axis(index=['Image', 'class'])
+tmp.name = 'People counting'
 tmp = tmp.reset_index()
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 4), dpi=200)
-sns.lineplot(tmp, x=tmp['filename'], y=tmp['value'], hue=tmp['class'],
+sns.lineplot(tmp, x=tmp['Image'], y=tmp['People counting'], hue=tmp['class'],
              markers=False, dashes=False, lw=1, palette=sns.color_palette("bright", 8), ax=ax)
 ax.set_title('Model predictions comparison')
 ax.set_xticklabels(ax.get_xticklabels(), rotation=90, size=8)
 ax.grid()
 plt.tight_layout()
 # plt.show()
-plt.savefig('./data/img_class_ground_truth/1.jpg')
+plt.savefig('./data/img_class_ground_truth/Model predictions comparison.jpg')
 
 print('')
--- models/utils/Consts.py
+++ models/utils/Consts.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 MODEL_LIST = np.array([
-    # 'DertResNet50', 'YOLOv8', 'insightface', 'SAHI_YOLOv8',
+    # 'DertResNet50',
+    # 'insightface',
+    # 'SAHI_YOLOv8',
+    # 'YOLOv8x_BB426_pre',
     'YOLOv8x_BB158_500_t16_b',
-    'YOLOv8x_BB158_1000_t24_l',
+    'YOLOv8x_BB426_760_t31_b',
 ])
 LABEL_PERSON = '1'
